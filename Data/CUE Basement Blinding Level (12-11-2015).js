67f534dfdcb5d250c3158bf3feabb8423e982297
kin525 <kfrecord2022@gmail.com>
2025-10-23 13:42:33 +0800

    add Data folder

---
+++ Data/CUE Basement Blinding Level (12-11-2015).js
@@ -0,0 +1,4 @@
+//CUE Basement Blinding Level (12-11-2015)
+var lineinput=new Array()
+lineinput[1]=["346.171","819762.742","813524.462","-5.663"]
+lineinput[2]=["375.121","819754.54","813552.226","-5.663"]
